088547f7da791f34d7e69db459a57338bec4a1f8
Clyde-Goodall <goodalluigi@gmail.com>
2024-09-05 01:14:48 -0400

    autocomplete suggestions added

--- src/views/CalcView.vue
+++ src/views/CalcView.vue
@@ -3,7 +3,12 @@
   <main>
     <div class="calc-container">
       <div class="search-row">
-        <input type="text" v-model="name" placeholder="e.g. Breloom" @keyup.enter="addToList()" /> <input type="button" @click="addToList()" value="+"/>
+        <input type="text" list="pokemon-complete" v-model="name" placeholder="e.g. Breloom" @keyup.enter="addToList()" /> <input type="button" @click="addToList()" value="+"/>
+        <datalist id="pokemon-complete">
+          <option v-for="l in datalistItems(name)" :key="l" :value="l"/>
+        </datalist>
+        <!-- <input type="text" v-model="searchString" placeholder="Search" @keyup.enter="search()" /> -->
+
       </div>
       <div v-if="pkmn.pokemon.length === 0 "></div>
       <div v-else class="items">
@@ -119,13 +124,33 @@
   import { ref } from 'vue';
   import { usePokemonStore } from '@/stores/pokemon';
   import ProgressBar from '../components/ProgressBar.vue';
+  import { pokemon } from '../assets/pokemon.js';
 
   const pkmn = usePokemonStore();
   let name = ref('');
+  let searchString = ref('');
 
   function addToList() {
     pkmn.addMon(name);
     name.value = '';
+  }
+
+  function pkmnListWithFilter() {
+    if(searchString) return pkmn.pokemon.slice().reverse()
+    let filteredList = pkmn.pokemon.filter(p => {
+      return p.name.includes(searchString);
+    })
+    return filteredList;
+  }
+
+  function datalistItems(str) {
+    let list = [];
+    for(let p of pokemon) {
+      let P = p.toUpperCase();
+      if(P.includes(str.toUpperCase())) list.push(p);
+    }
+    console.log(list);
+    return list;
   }
 
 </script>
